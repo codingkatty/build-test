e32617eae9c6b21bb16de3b5e7e543b7f2541cef
Violetz <m001747@permatapintar.ukm.edu.my>
2025-03-11 20:43:24 +0800

    adding button

--- sketch.js
+++ sketch.js
@@ -3,3 +3,19 @@ function setup() {
     myCanvas.parent('game-container');
     background(255, 255, 255);
 }
+
+draw = function() {
+    fill(21, 62, 100); // start color
+
+    if (mouseIsPressed && mouseX > 330 && mouseX < 580 && mouseY > 400 && mouseY < 500) { 
+        fill(25, 99, 168); // click color
+    }
+    noStroke();
+    rect(330, 400, 250, 100, 40);  // the button
+
+    // The button text
+    fill(255, 255, 255);
+    textSize(30);
+    textAlign(CENTER, CENTER); // Center the text
+    text("Start", 330 + 250 / 2, 400 + 100 / 2); // Centering the text inside the button
+};
